f561bab294718ea57854ae982b1fb3e22576deef
Loys <abeetlott@gmail.com>
2018-06-30 17:56:15 +0200

    unity project init with visual elements
    + Canvas / buttons / texts
    + some tests with tileMap

---
+++ lois-brun-test-st/Assets/Scripts/GameObjects/GridRenderingManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GridRenderingManager : MonoBehaviour {
+
+	public Tilemap tileMap;
+	public Tile tile1;
+	// Use this for initialization
+	void Start () {
+		tileMap.SetTile(new Vector3Int(10,2,0), tile1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
